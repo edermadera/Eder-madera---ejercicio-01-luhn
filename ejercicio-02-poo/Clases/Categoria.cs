ba03b8d0d9374fd9b52e969358a7c40a99b1512b
edermadera <ederdejesusmadera12@gmail.com>
2026-07-27 16:21:17 -0500

    Categoria.cs

--- ejercicio-02-poo/Clases/Categoria.cs
+++ ejercicio-02-poo/Clases/Categoria.cs
@@ -1,1 +1,20 @@
+public class ejercicio_02poo
+{
+    public string Nombre { get; set; }
 
+    public string Color { get; set; }
+
+    public string Descripcion { get; set; }
+
+    public Categoria()
+    {
+
+    }
+
+    public Categoria(string nombre, string color, string descripcion)
+    {
+        Nombre = nombre;
+        Color = color;
+        Descripcion = descripcion;
+    }
+}
