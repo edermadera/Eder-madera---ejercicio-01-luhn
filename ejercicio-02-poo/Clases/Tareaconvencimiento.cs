a33e426239c7f912816bb1bf389dced6d6e0a7ac
edermadera <ederdejesusmadera12@gmail.com>
2026-07-27 16:26:41 -0500

    Tareaconvencimiento.cs

--- ejercicio-02-poo/Clases/Tareaconvencimiento.cs
+++ ejercicio-02-poo/Clases/Tareaconvencimiento.cs
@@ -1,1 +1,31 @@
+  public class TareaConVencimiento : Tarea
+    {
+       
+        public DateTime FechaVencimiento { get; set; }
 
+      
+        public int DiasRestantes
+        {
+            get
+            {
+                return (FechaVencimiento.Date - DateTime.Now.Date).Days;
+            }
+        }
+
+       
+        public TareaConVencimiento() : base()
+        {
+            FechaVencimiento = DateTime.Now.AddDays(7);
+        }
+
+            public override void MostrarInfo()
+        {
+            
+            base.MostrarInfo();
+
+            Console.WriteLine($"Fecha de vencimiento: {FechaVencimiento:d}");
+            Console.WriteLine($"Días restantes: {DiasRestantes}");
+           
+        }
+    }
+}
